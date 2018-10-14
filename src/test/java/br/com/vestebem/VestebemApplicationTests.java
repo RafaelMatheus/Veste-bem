3bc0ebcdb31003b3c01c225611a7a99c575e08bf
RafaelMatheus <rafaelmatheusdecastro@hotmail.com.br>
2018-10-14 13:42:57 -0300

    Adicionado primeiro commit

---
+++ src/test/java/br/com/vestebem/VestebemApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vestebem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VestebemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
