970bb1e170a63ca9e799b4e9c7cda89dae6f4795
RafaelMatheus <rafaelmatheusdecastro@hotmail.com.br>
2018-10-14 22:02:47 -0300

    Adicioado mapeamento entre endereco e cliente

--- src/main/java/br/com/vestebem/VestebemApplication.java
+++ src/main/java/br/com/vestebem/VestebemApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.vestebem.model.Categoria;
 import br.com.vestebem.model.Cidade;
+import br.com.vestebem.model.Cliente;
+import br.com.vestebem.model.Endereco;
 import br.com.vestebem.model.Estado;
 import br.com.vestebem.model.Produto;
+import br.com.vestebem.model.enums.TipoCliente;
 import br.com.vestebem.repositories.CategoriaRepository;
 import br.com.vestebem.repositories.CidadeRepository;
+import br.com.vestebem.repositories.ClienteRepository;
+import br.com.vestebem.repositories.EnderecoRepository;
 import br.com.vestebem.repositories.EstadoRepository;
 import br.com.vestebem.repositories.ProdutoRepository;
 
@@ -26,7 +31,11 @@ public class VestebemApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VestebemApplication.class, args);
 
@@ -49,21 +58,41 @@ public class VestebemApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2, p3));
 		
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
 		
-		Cidade c1 = new Cidade(null, "Uberlandia", est1);
-		Cidade c2 = new Cidade(null, "São Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		
+		
+		Estado est1 = new Estado(null, "Pernambuco");
+		Estado est2 = new Estado(null, "Paraiba");
+		
+		Cidade c1 = new Cidade(null, "Itambé", est1);
+		Cidade c2 = new Cidade(null, "Pedras de fogo", est2);
+		Cidade c3 = new Cidade(null, "João Pessoa", est2);
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		
+		Cliente cli1 = new Cliente(null, "Rafael", "rafael@hotmail.com.br","111.111.111-11", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("2222-2222", "3333-3333"));
+		
+		Cliente cli2 = new Cliente(null, "Debora", "debora@hotmail.com.br","111.111.111-11", TipoCliente.PESSOAFISICA);
+		cli2.getTelefones().addAll(Arrays.asList("2222-2222", "3333-3333"));
+		
+		Endereco e1 = new Endereco(null, "Eliud", "140", "", "Centro", "559200-000", cli1, c1);
+		Endereco e2 = new Endereco(null, "Primeiro de janeiro", "283", "", "Centro", "58328-000", cli1, c2);
+		Endereco e3 = new Endereco(null, "Não sei o nome", "Não sei", "Próximo ao waldecyr", "Centro", "58328-000", cli2, c2);
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
+		
 	}
 }
