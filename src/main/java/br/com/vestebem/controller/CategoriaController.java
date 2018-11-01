175a0bd1ad08141cd17add359ae382a323230a32
RafaelMatheus <rafaelmatheusdecastro@hotmail.com.br>
2018-11-01 11:17:35 -0300

    autorizando endpoints para perfis epecificos

--- src/main/java/br/com/vestebem/controller/CategoriaController.java
+++ src/main/java/br/com/vestebem/controller/CategoriaController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,36 +35,7 @@ public class CategoriaController {
 				.collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
-		Categoria categoria = categoriaService.findById(id);
-		return ResponseEntity.ok().body(categoria);
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDto categoriaDto) {
-		Categoria categoria = categoriaService.fromDto(categoriaDto);
-		categoria = categoriaService.insert(categoria);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
-				.toUri();
-		return ResponseEntity.created(uri).build();
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDto categoriaDto, @PathVariable Integer id) {
-		Categoria categoria = categoriaService.fromDto(categoriaDto);
-		categoria.setId(id);
-		categoria = categoriaService.update(categoria);
-		return ResponseEntity.noContent().build();
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-		categoriaService.delete(id);
-		return ResponseEntity.noContent().build();
-	}
-
+	
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDto>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
@@ -72,6 +44,37 @@ public class CategoriaController {
 		Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
 		Page<CategoriaDto> listDto = list.map(obj -> new CategoriaDto(obj));
 		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+		Categoria categoria = categoriaService.findById(id);
+		return ResponseEntity.ok().body(categoria);
+	}
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDto categoriaDto) {
+		Categoria categoria = categoriaService.fromDto(categoriaDto);
+		categoria = categoriaService.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDto categoriaDto, @PathVariable Integer id) {
+		Categoria categoria = categoriaService.fromDto(categoriaDto);
+		categoria.setId(id);
+		categoria = categoriaService.update(categoria);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		categoriaService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
