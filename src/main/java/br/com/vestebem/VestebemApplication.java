63c7283f12c10654bc6827360b1794f12aa2bbf7
RafaelMatheus <rafaelmatheusdecastro@hotmail.com.br>
2018-11-01 16:14:14 -0300

    Aplicação salvando imagem no na amazon

--- src/main/java/br/com/vestebem/VestebemApplication.java
+++ src/main/java/br/com/vestebem/VestebemApplication.java
@@ -1,12 +1,16 @@
 package br.com.vestebem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.vestebem.service.S3Service;
+
 @SpringBootApplication
 public class VestebemApplication implements CommandLineRunner {
-	
+	@Autowired
+	private S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(VestebemApplication.class, args);
 
@@ -14,6 +18,6 @@ public class VestebemApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("/home/rafael/Imagens/Wallpapers/imagem.jpg");
 	}
 }
